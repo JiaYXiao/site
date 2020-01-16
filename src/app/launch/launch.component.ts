d47a9a4d4bad562ebe094a8e6f05273f4197e0b1
JiaYXiao <blah_647@hotmail.com>
2020-01-15 23:34:16 -0500

    added entrance animation

--- src/app/launch/launch.component.ts
+++ src/app/launch/launch.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-// import { trigger, style, animate, transition } from '@angular/animations';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-launch',
-  // animations: [
-  //   trigger(
-  //     'enterAnimation', [
-  //       transition(':enter', [
-  //         style({transform: 'translateX(100%)', opacity: 0}),
-  //         animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
-  //       ]),
-  //       transition(':leave', [
-  //         style({transform: 'translateX(0)', opacity: 1}),
-  //         animate('500ms', style({transform: 'translateX(100%)', opacity: 0}))
-  //       ])
-  //     ]
-  //   )
-  // ],
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateX(100%)', opacity: 0}),
+          animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)', opacity: 1}),
+          animate('500ms', style({transform: 'translateX(100%)', opacity: 0}))
+        ])
+      ]
+    )
+  ],
   templateUrl: './launch.component.html',
   styleUrls: ['../../sass/launch.component.scss']
 })
